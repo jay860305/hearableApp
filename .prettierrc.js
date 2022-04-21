87e4da7631663a074986e3eab2b2caebb28e961c
jay860305 <jay@bunnit.kr>
2022-04-21 11:32:12 +0900

    project setting files

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'es5',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 120,
+  semi: true,
+  arrowParens: 'always',
+};
